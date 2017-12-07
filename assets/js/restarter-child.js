e3eea78000cfc78f2d7675028b334d5318a0b369
Mahdi Yazdani <mahdiyazdani@mail.com>
2017-12-07 17:30:54 +0400

    Add files via upload

---
+++ assets/js/restarter-child.js
@@ -0,0 +1,11 @@
+/**
+ * Restarter Child Theme
+ *
+ * @author      Mahdi Yazdani
+ * @package     Restarter Child
+ * @since       1.0.0
+ */
+jQuery(document).ready(function($) {
+    'use strict';
+
+}); /*Document Ready End*/
